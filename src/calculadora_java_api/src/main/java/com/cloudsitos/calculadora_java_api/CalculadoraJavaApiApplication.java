16d89f9bb22cfac881ba67dc4f6c5be3a9fad0e3
JuanPinoC <juan.pino@tecsup.edu.pe>
2022-08-09 12:25:48 -0500

    Initial commit

---
+++ src/calculadora_java_api/src/main/java/com/cloudsitos/calculadora_java_api/CalculadoraJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cloudsitos.calculadora_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/calculadora_java_api/src/test/java/com/cloudsitos/calculadora_java_api/CalculadoraJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudsitos.calculadora_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
