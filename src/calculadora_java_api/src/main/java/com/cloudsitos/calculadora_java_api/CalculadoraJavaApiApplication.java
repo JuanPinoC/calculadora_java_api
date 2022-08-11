4abd820e8cff36670d63c8eb25d4573a4a93d895
Carlos Alberto Taipe Casas <ctaipeca@emeal.nttdata.com>
2022-08-11 10:32:49 -0500

    Agrego libreria Math

--- src/calculadora_java_api/src/main/java/com/cloudsitos/calculadora_java_api/CalculadoraJavaApiApplication.java
+++ src/calculadora_java_api/src/main/java/com/cloudsitos/calculadora_java_api/CalculadoraJavaApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadoraJavaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadoraJavaApiApplication.class, args);
 	}
-
 }
